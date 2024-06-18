ff0ac6c5d97c7066a342e793e43f6fd3e49dd919
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-18 08:45:49 -0700

    Create populatedb file

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
 // First we import (require) useful node libraries that we already downloaded using npm
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-// Then we import our routers from the routes folder. Currently 2 routes. 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Then we import our routers from the routes folder. Currently 2 routes.
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 // Finally create the app object
 var app = express();
 
-
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://kevinfboutilier:AMRPU6oeOZv8fCKW@locallibrary.o1xynnt.mongodb.net/?retryWrites=true&w=majority&appName=localLibrary";
+const mongoDB =
+  "mongodb+srv://kevinfboutilier:AMRPU6oeOZv8fCKW@locallibrary.o1xynnt.mongodb.net/?retryWrites=true&w=majority&appName=localLibrary";
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
-
-
-
-
-
 // Use our app object to set up the view (template) engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-// These functions add the middleware libraries that we imported above into the request handling chain. 
-app.use(logger('dev'));
+// These functions add the middleware libraries that we imported above into the request handling chain.
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // After middleware we add route handling for our previously imported routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
@@ -67,4 +62,7 @@ module.exports = app;
 // otherwise try:
 // mongodb+srv://kevinfboutilier:AMRPU6oeOZv8fCKW@locallibrary.o1xynnt.mongodb.net/local_library?retryWrites=true&w=majority&appName=localLibrary
 // It's possible the capital O in the password above is a zero instead...
+//  ************************************************************************************************************************************************************************
+//  ************************************************************************************************************************************************************************
+// SHIT. Our connection string isn't working. We gotta backtrack....... HARD.
 //  ************************************************************************************************************************************************************************
