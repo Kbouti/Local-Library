4d98c90c612dd1b4b90356a418195951bb993891
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-21 16:57:07 -0700

    Create lifespan virtual property and implement it

--- models/author.js
+++ models/author.js
@@ -1,6 +1,5 @@
 const { DateTime } = require("luxon");
 
-
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -20,9 +19,41 @@ AuthorSchema.virtual("name").get(function () {
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
   }
-
   return fullname;
 });
+
+// ***********************************************************************************************
+// Next they want us to create a new virtual property for lifespan, just like they've done for full name.
+AuthorSchema.virtual("lifespan").get(function () {
+  let birthDate_formatted = "??";
+  let deathDate_formatted = "??";
+// let lifespan
+  if (this.date_of_birth && this.date_of_death) {
+    console.log(`Has both dates`)
+    birthDate_formatted = DateTime.fromJSDate(
+      this.date_of_birth
+    ).toLocaleString(DateTime.DATE_MED);
+    deathDate_formatted = DateTime.fromJSDate(
+      this.date_of_death
+    ).toLocaleString(DateTime.DATE_MED);
+    console.log(`birthDate_formatted: ${birthDate_formatted}`)
+  } else if (this.date_of_birth) {
+    birthDate_formatted = DateTime.fromJSDate(
+      this.date_of_birth
+    ).toLocaleString(DateTime.DATE_MED);
+  } else if (this.date_of_death) {
+    deathDate_formatted = DateTime.fromJSDate(
+      this.date_of_death
+    ).toLocaleString(DateTime.DATE_MED);
+  } else {
+    let lifespan = "Lifespan Unknown";
+    return lifespan;
+  }
+  let lifespan = `${birthDate_formatted} - ${deathDate_formatted}`;
+  return lifespan;
+});
+
+// ***********************************************************************************************
 
 // Virtual for author's URL
 AuthorSchema.virtual("url").get(function () {
@@ -30,17 +61,17 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
-
-
 AuthorSchema.virtual("birthDate_formatted").get(function () {
-  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
 });
 
 AuthorSchema.virtual("deathDate_formatted").get(function () {
-  return DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
 });
-
-
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
