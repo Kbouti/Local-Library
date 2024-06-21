0018f539ed4ea51a9e500f5edb9d490e30d23e7c
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-21 09:45:37 -0700

    Attempt to format author dates

--- models/author.js
+++ models/author.js
@@ -1,3 +1,6 @@
+const { DateTime } = require("luxon");
+
+
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -26,6 +29,18 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+
+
+AuthorSchema.virtual("birthDate_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+});
+
+AuthorSchema.virtual("deathDate_formatted").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+});
+
+
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
