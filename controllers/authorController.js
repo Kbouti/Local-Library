e412c71562bc343faf15b77288de586ef2b62003
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-26 12:37:00 -0700

    Complete author update post controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -98,7 +98,6 @@ exports.author_create_post = [
   }),
 ];
 
-
 // Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
   // Get details of author and all their books (in parallel)
@@ -118,7 +117,6 @@ exports.author_delete_get = asyncHandler(async (req, res, next) => {
     author_books: allBooksByAuthor,
   });
 });
-
 
 // Handle Author delete on POST.
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
@@ -143,7 +141,6 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
   }
 });
 
-
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
   // res.send("NOT IMPLEMENTED: Author update GET");
@@ -158,13 +155,64 @@ exports.author_update_get = asyncHandler(async (req, res, next) => {
 
   res.render("author_form", {
     title: "Update Author",
-    author: author
-  })
-
-
+    author: author,
+  });
 });
 
 // Handle Author update on POST.
-exports.author_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-});
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedAuthor = await Author.findByIdAndUpdate(
+        req.params.id,
+        author,
+        {}
+      );
+      res.redirect(updatedAuthor.url);
+    }
+  }),
+];
